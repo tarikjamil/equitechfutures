41fe217997d1c6476ad8ae7bc44f3183c7445108
tarikjamil <tarikjamill@gmail.com>
2023-10-07 20:02:59 +0100

    Update alumnis.js

--- alumnis.js
+++ alumnis.js
@@ -1,51 +1,29 @@
-// Create a variable for the API endpoint. In this example, we're accessing Xano's API
 let xanoUrl = new URL("https://x8ki-letl-twmt.n7.xano.io/api:v0K9JRjG");
 
-// Define a function (set of operations) to get restaurant information.
-// This will use the GET request on the URL endpoint
 function getAlumnis2() {
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  // XMLHttpRequest is the standard way you access an API in plain Javascript.
   let request = new XMLHttpRequest();
-
-  // Define a function (set of operations) to get restaurant information.
-  // Creates a variable that will take the URL from above and makes sure it displays as a string.
-  // We then add the word 'restaurant" so the API endpoint becomes https://x715-fe9c-6426.n7.xano.io/api:Iw1iInWB/restaurant
   let url = `${xanoUrl}/alumnis2`;
-
-  // Remember the 'request' was defined above as the standard way to access an API in Javascript.
-  // GET is the verb we're using to GET data from Xano
   request.open("GET", url, true);
 
-  // When the 'request' or API request loads, do the following...
   request.onload = function () {
-    // Store what we get back from the Xano API as a variable called 'data' and converts it to a javascript object
     let data = JSON.parse(this.response);
 
-    // Status 200 = Success. Status 400 = Problem.  This says if it's successful and no problems, then execute
     if (request.status >= 200 && request.status < 400) {
-      // Map a variable called cardContainer to the Webflow element called "Cards-Container"
       let cardContainer = document.getElementById("Cards-Container");
 
-      // This is called a For Loop. This goes through each object being passed back from the Xano API and does something.
-      // Specifically, it says "For every element in Data (response from API), call each individual item restaurant"
       data.forEach((alumnis2) => {
-        // For each restaurant, create a div called card and style with the "Sample Card" class
         let style = document.getElementById("samplestyle");
-        // Copy the card and it's style
         let card = style.cloneNode(true);
 
         card.setAttribute("id", "");
         card.style.display = "block";
 
-        // When a restuarant card is clicked, navigate to the item page by passing the restaurant id
         card.addEventListener("click", function () {
           document.location.href = "/alumnisos?id=" + alumnis2.id;
         });
 
-        // For each restaurant, Create an image and use the restaurant image coming from the API
         let img = card.getElementsByTagName("IMG")[0];
-        img.src = alumnis2.image + "?tpl=big:box"; //
+        img.src = alumnis2.image + "?tpl=big:box";
 
         let h2 = card.getElementsByTagName("H2")[0];
         h2.textContent = alumnis2.Name;
@@ -62,18 +40,17 @@ function getAlumnis2() {
         let programname = card.getElementsByClassName("program-name")[0];
         programname.textContent = alumnis2.Programs;
 
-        // Place the card into the div "Cards-Container"
-
         cardContainer.appendChild(card);
       });
+
+      // Populate the select options after the cards are added.
+      populateSelectOptions();
     }
   };
 
-  // Send Restaurant request to API
   request.send();
 }
 
-// This fires all of the defined functions when the document is "ready" or loaded
 (function () {
   getAlumnis2();
 })();
@@ -141,6 +118,3 @@ function addOptionsToSelect(selectId, items) {
     select.appendChild(option);
   });
 }
-
-// Call the populate function when the document is fully loaded.
-document.addEventListener("DOMContentLoaded", populateSelectOptions);
